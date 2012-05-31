890d8211bcfe178824c96f616f3f416cdd61ce2f
Ben Gamari <bgamari.foss@gmail.com>
2012-05-31 11:03:25 -0400

    Beta: Use Unboxed vectors

--- Numeric/MixtureModel/Beta.hs
+++ Numeric/MixtureModel/Beta.hs
@@ -26,11 +26,12 @@ module Numeric.MixtureModel.Beta ( -- * General data types
                                  ) where
                               
 import           Data.Function (on)       
-import qualified Data.Vector as V
-import qualified Data.Vector.Mutable as MV
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Unboxed.Mutable as MV
 import           Control.Monad.ST       
 
 import           Data.Number.LogFloat hiding (realToFrac, isInfinite)
+import           Data.Number.LogFloat.Vector
 import           Numeric.SpecFunctions (logBeta)
 import           Statistics.Sample (meanVarianceUnb)
        
