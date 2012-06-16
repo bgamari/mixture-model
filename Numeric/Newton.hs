3e6b13da333c3fa5025b2e3d8923f0c7fd9fc883
Ben Gamari <bgamari.foss@gmail.com>
2012-06-16 21:02:33 +0200

    Newton: Add type signature for findRoot

--- Numeric/Newton.hs
+++ Numeric/Newton.hs
@@ -5,7 +5,7 @@ module Numeric.Newton ( findRoot
 type Objective a = a -> a
 type Derivative a = a -> a
 
-numericalDeriv :: (RealFrac a, Show a) => a -> Objective a -> a -> a -> a
+numericalDeriv :: (RealFrac a) => a -> Objective a -> a -> a -> a
 numericalDeriv _ _ h _ | h < 1e-14 = error "numericalDeriv: Derivative underflow"
 numericalDeriv tol f h x =
     let deriv h = (f (x+h) - f x) / h
@@ -15,15 +15,16 @@ numericalDeriv tol f h x =
               then numericalDeriv tol f (h/2) x
               else dfSmall
 
-newton :: (RealFrac a, Show a) => Objective a -> Derivative a -> a -> [a]
+newton :: (RealFrac a) => Objective a -> Derivative a -> a -> [a]
 newton f fDeriv x0 =
     let x = x0 - f x0 / fDeriv x0
     in x : newton f fDeriv x
 
-findRoot' :: (RealFrac a, Show a) => Int -> a -> Objective a -> Derivative a -> a -> a
+findRoot' :: (RealFrac a) => Int -> a -> Objective a -> Derivative a -> a -> a
 findRoot' maxIter tol f fDeriv x0 =
     case dropWhile (\x->abs (f x) > tol) $ take maxIter $ newton f fDeriv x0 of
       []  -> error "findRoot': Failed to find root"
       x:_ -> x
     
+findRoot :: (RealFrac a) => a -> Objective a -> Derivative a -> a -> a
 findRoot = findRoot' 1000
